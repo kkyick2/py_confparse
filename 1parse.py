b8ec221879bff5d1e40c95e36ecbae1837c91196
kkyick2 <kkyick2@gmail.com>
2023-06-09 09:53:08 +0000

    20230609 1753

--- 1parse.py
+++ 1parse.py
@@ -4,43 +4,42 @@ import pandas as pd
 from ciscoconfparse import CiscoConfParse
 
 
-
-def parseConfig(inf):
+def parseCiscoIOS_Interface(parse, hostname):
     '''
-        @inf : input config txt file
-        @rtl: return a list of dict
+        @parse: ciscoconfparse Object
+        @rtl: return a list of Interface dict
             [{'hostname': 'ES-B15W-1', 'interface': 'interface Loopback0', 'shutdown': '-', 'description': '-'}, 
              {'hostname': 'ES-B15W-1', 'interface': 'interface Loopback0', 'shutdown': '-', 'description': '-'}, ...]
     '''
-    rtl = []
-    parse = CiscoConfParse(inf)
-    #################################################
-    # hostname 
-    hostname_obj = parse.find_objects(r'^hostname')[0]
-    hostname = hostname_obj.re_match_typed(r'^hostname\s+(\S+)', default='')
-    print("--hostname: {}".format(hostname))
-    #################################################
     # all_intfs: list of intf objects example:
     # [ <IOSCfgLine # 258 'interface GigabitEthernet1/1'>, 
-    #   <IOSCfgLine # 261 'interface GigabitEthernet1/2'>, ...]    
+    #   <IOSCfgLine # 261 'interface GigabitEthernet1/2'>, ...] 
+    rtl = []
     all_intfs = parse.find_objects(r"^interface ")
     for intf_obj in all_intfs: 
 
+        l_nosw_noip = []
+        l_trunk_vlan_list = []
+        l_stp = []
         l_iphelper = []
         l_standby = []
         l_ippim = []
         l_ipigmp = []
         l_noipxxx = []
+        l_ippolicy = []
+        l_servicepolicy = []
         l_oth_cmd = []
         row = {
             'hostname' : '-',
             'interface' : '-',
             'shutdown' : '-',
             'description' : '-',
+            'nosw_noip': '-',
             'port_mode' : '-',
             'trunk vlan': '-',
             'access vlan': '-',
             'channel-group': '-',
+            'spanning-tree': '-',
             'ipaddr': '-',
             'vrf': '-',
             'helper-address' : '-',
@@ -48,6 +47,8 @@ def parseConfig(inf):
             'standby': '-',
             'ippim': '-',
             'ipigmp': '-',
+            'ippolicy': '-',
+            'servicepolicy': '-',
             'no ip xxx': '-',
             'other': '-',
         }
@@ -57,7 +58,7 @@ def parseConfig(inf):
         # interface
         intf2 = re.split('interface ', intf_obj.text)[1]
         row['interface'] = intf2
-        # print("--interface: {}".format(intf2))
+        #print("--interface: {}".format(intf2))
         #################################################
         # print("----intf_child: {}".format(intf_obj.children))
         for intf_child in intf_obj.children:
@@ -75,7 +76,11 @@ def parseConfig(inf):
                 row['description'] = line2[1]
 
             #################################################
-            # switchport mode
+            # [switchport/no switchport/no ip address]
+            elif re.compile('no switchport$|switchport$|no ip address$').match(line):
+                l_nosw_noip.append(line)
+                
+            # switchport mode [access\trunk]
             elif line.startswith('switchport mode'):
                 line2 = re.split('switchport mode ',line)
                 row['port_mode'] = line2[1]
@@ -91,8 +96,16 @@ def parseConfig(inf):
 
             # switchport trunk allowed vlan
             elif line.startswith('switchport trunk allowed vlan'):
-                line2 = re.split('switchport trunk allowed vlan ',line)
-                row['trunk vlan'] = line2[1]
+                line2 = re.split('switchport trunk allowed vlan ', line)
+                #row['trunk vlan'] = line2[1]
+                #print(line2[1]
+                l_trunk_vlan_list.append(line2[1])
+
+            # spanning-tree xxx
+            # multiple match, put in list
+            elif line.startswith('spanning-tree'):
+                line2 = re.findall(r'spanning-tree\s+(.+)', line)
+                l_stp.append(line2[0])
 
             #################################################
             # ip address
@@ -134,17 +147,35 @@ def parseConfig(inf):
                 line2 = re.findall(r'ip igmp\s+(.+)', line)
                 l_ipigmp.append(line2[0])
 
-            # no ip redirects, no ip unreachables, no ip proxy-arp
+            # ip policy
             # multiple match, put in list
-            elif line.startswith('no ip redirects') | line.startswith('no ip unreachables') | line.startswith('no ip proxy-arp'):
-                line2 = re.findall(r'no\s+ip\s+(.+)', line)
-                l_noipxxx.append(line2[0])
+            elif line.startswith('ip policy'):
+                line2 = re.findall(r'ip policy\s+(.+)', line)
+                l_ippolicy.append(line2[0])
+
+            # service policy
+            # multiple match, put in list
+            elif line.startswith('service-policy'):
+                line2 = re.findall(r'service-policy\s+(.+)', line)
+                l_servicepolicy.append(line2[0])
+
+            # [no ip redirects/no ip unreachables/no ip proxy-arp]
+            # multiple match, put in list
+            elif re.compile('no ip redirects$|no ip unreachables$|no ip proxy-arp$').match(line):
+                l_noipxxx.append(line)
+
             #################################################
             # other child in intf
             else:
                 l_oth_cmd.append(line)
 
         # Append row to list d
+        if l_nosw_noip:
+            row['nosw_noip'] = l_nosw_noip
+        if l_trunk_vlan_list:
+            row['trunk vlan'] = l_trunk_vlan_list
+        if l_stp:
+            row['spanning-tree'] = l_stp
         if l_iphelper:
             row['helper-address'] = l_iphelper
         if l_standby:
@@ -153,14 +184,51 @@ def parseConfig(inf):
             row['ippim'] = l_ippim
         if l_ipigmp:
             row['ipigmp'] = l_ipigmp
+        if l_ippolicy:
+            row['ippolicy'] = l_ippolicy
+        if l_servicepolicy:
+            row['servicepolicy'] = l_servicepolicy
         if l_noipxxx:
             row['no ip xxx'] = l_noipxxx
         if l_oth_cmd:
             row['other'] = l_oth_cmd
         rtl.append(row)
-    
+
     return rtl
 
+def parseConfig(inf):
+    '''
+        @inf : input config txt file
+        @rt: return a list of dict
+            [{'hostname': 'ES-B15W-1', 'interface': 'interface Loopback0', 'shutdown': '-', 'description': '-'}, 
+             {'hostname': 'ES-B15W-1', 'interface': 'interface Loopback0', 'shutdown': '-', 'description': '-'}, ...]
+    '''
+    parse = CiscoConfParse(inf)
+    #################################################
+    # Hostname line
+    line = parse.find_objects(r'^hostname\b|^\s+sysname')[0].text
+    type = 'tbc'
+
+    # Cisco: hostname<space>SW1
+    if line.startswith('hostname'):
+        hostname = re.findall(r'^hostname\s+(\S+)', line)[0]
+        type = 'ios'
+
+    # H3C: <space>sysname<space>SW1
+    elif line.startswith(' sysname'):
+        hostname = re.findall(r'^\s+sysname\s+(\S+)', line)[0]
+        type = 'h3c'
+
+    print("--hostname: {} | type: {}".format(hostname, type))
+    #################################################
+    # parseCiscoIOS_Interface
+    rt_int = []
+    if type == 'ios':
+        rt_int = parseCiscoIOS_Interface(parse, hostname)
+    elif type == 'h3c':
+        rt_int = parseCiscoIOS_Interface(parse, hostname)
+
+    return rt_int
 
 def output_dataframe(df, outf):
     '''
@@ -201,18 +269,17 @@ def start_script(input_cfg):
 
 def main():
     input_cfg_list = [
+        'ES-B10-1.cfg',
+        'hkstp-core-sw.cfg',
         'ES-B15W-1.cfg',
         'ES-B15W-2.cfg',
         'ES-B16W-1.cfg',
         'ES-B16W-2.cfg',
+        'CS-1.cfg',
     ]
     for input_cfg in input_cfg_list:
         df = start_script(input_cfg)
-
-
-
-
-    merage_dataframe()
+        
 
 if __name__ == "__main__":
     main()
